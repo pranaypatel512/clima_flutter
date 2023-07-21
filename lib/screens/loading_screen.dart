fe164c0169c3e5cef909cfbb12d65a6e893609da
pRaNaY <www.pranay1337.com@gmail.com>
2023-07-21 18:20:30 +0530

    refactor location access with class

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,8 @@
+import 'package:clima_flutter/location.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+
+import '../logger.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -38,40 +41,10 @@ class _LoadingScreenState extends State<LoadingScreen> {
     );
   }
 
-  void _determinePosition() async {
-    bool serviceEnabled;
-    LocationPermission permission;
-
-    // Test if location services are enabled.
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      // Location services are not enabled don't continue
-      // accessing the position and request users of the
-      // App to enable the location services.
-      return Future.error('Location services are disabled.');
-    }
-
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        // Permissions are denied, next time you could try
-        // requesting permissions again (this is also where
-        // Android's shouldShowRequestPermissionRationale
-        // returned true. According to Android guidelines
-        // your App should show an explanatory UI now.
-        return Future.error('Location permissions are denied');
-      }
-    }
-
-    if (permission == LocationPermission.deniedForever) {
-      // Permissions are denied forever, handle appropriately.
-      return Future.error(
-          'Location permissions are permanently denied, we cannot request permissions.');
-    }
-
-    // When we reach here, permissions are granted and we can
-    // continue accessing the position of the device.
-    _Position = await Geolocator.getCurrentPosition();
+  void getLocation() async {
+    Location location = Location();
+    location.latitude;
+    location.longitude;
+    log("${location.latitude}:${location.longitude}");
   }
 }
