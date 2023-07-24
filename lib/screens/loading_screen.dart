ddf47da8f22045b96c356e67a97efccf8f75b4c1
pRaNaY <www.pranay1337.com@gmail.com>
2023-07-24 14:33:41 +0530

    get request basics

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:clima_flutter/location.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
 
 import '../logger.dart';
 
@@ -10,7 +13,6 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-
   @override
   void initState() {
     super.initState();
@@ -19,6 +21,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold(
       body: SafeArea(
         child: Center(
@@ -32,7 +35,6 @@ class _LoadingScreenState extends State<LoadingScreen> {
                     // });
                   },
                   child: Text('Get Location')),
-              Text(_Position.toString())
             ],
           ),
         ),
@@ -45,5 +47,16 @@ class _LoadingScreenState extends State<LoadingScreen> {
     location.latitude;
     location.longitude;
     log("${location.latitude}:${location.longitude}");
+  }
+
+  void getData() async {
+    http.Response response = await http.get(Uri.parse(
+        'https://api.openweathermap.org/data/2.5/weather?lat=23.0225&lon=72.5714&appid=ef7a6d795a6fc79ed30ef4a9dfede057'));
+    log(response.body);
+
+    var id = jsonDecode(response.body)['weather'][0]['id'];
+    var temp = jsonDecode(response.body)['main']['temp'];
+    var name = jsonDecode(response.body)['name'];
+    log('$id:$temp:$name');
   }
 }
